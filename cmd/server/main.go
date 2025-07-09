b9f4bd3075d2c0bdd5113b25bd2658565f92ae6c
linuxer77 <harshitg187@gmail.com>
2025-07-09 19:13:31 +0530

    basic file strucutre

---
+++ cmd/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This shit is working now."))
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/", handleIndex)
+
+	fmt.Println("Port :8080 is active....")
+	http.ListenAndServe(":8080", r)
+}
