edaf324663016cfae2c85a87afae60b7decf29a3
linuxer77 <harshitg187@gmail.com>
2025-07-10 17:43:13 +0530

    basic contract established.

--- contract_logic/main.py
+++ contract_logic/main.py
@@ -11,13 +11,29 @@ class ERC721:
     
 class veriCredNFT(ERC721):
     tokenId = 0
+    verifiedOrg = {}
     
     def __init__(self):
         self.collectionName = "document"
         self.symbol = "VCRD"
+        self.msg_sender = "0x416c696365000000000000000000000000000000000000000000000000000000"
 
     def mintDoc(self, address, tokenURI):
-        tokenId += 1
-        self._mint(address, tokenURI)
-        self._setTokenURI(itemId, address)
-        return tokenId
+        
+        try:
+            if self.verifiedOrg[self.msg_sender]:            
+                tokenId += 1
+                self._mint(address, tokenURI)
+                self._setTokenURI(tokenId, address)
+                return tokenId
+            else:
+                return "University not verified."
+            
+        except Exception as e:
+            return "University is not listed."
+
+    def newOrg(self, orgAddress):
+        
+        # if msg_sender == owner:
+        self.verifiedOrg[orgAddress] = True
+        return
--- contracts/mint.sol
+++ contracts/mint.sol
@@ -5,18 +5,29 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract vericredNFT is ERC721URIStorage {
+    address public owner;
     uint256 private _tokenIds;
+    mapping(address => bool) public verifiedOrgs;
 
-    constructor() ERC721("certificate", "VCRD") {}
+    constructor() ERC721("document", "VCRD") {
+        owner = msg.sender;
+    }
 
     function mintDoc(
         address user,
         string memory tokenURI
     ) public returns (uint256) {
+        require(verifiedOrgs[msg.sender], "University is not verified/listed");
+
         _tokenIds += 1;
         uint256 newItemId = _tokenIds;
         _mint(user, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
+    }
+
+    function newOrg(address _org) public {
+        require(msg.sender == owner, "Only the owner can push new orgs.");
+        verifiedOrgs[_org] = true;
     }
 }
