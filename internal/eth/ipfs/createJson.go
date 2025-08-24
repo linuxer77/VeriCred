382cc7b9124ba2f584a6c48c666dd311d1cc5c0e
linuxer77 <harshitg187@gmail.com>
2025-08-24 20:38:54 +0530

    ipfs temp dir upload

--- internal/eth/ipfs/createJson.go
+++ internal/eth/ipfs/createJson.go
@@ -2,76 +2,42 @@ package ipfs
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
-// func CreateJSONFileAndStoreToIPFS() {
-
-// 	f, err := os.CreateTemp("/home/linuxer77/temp-files", "tempfile-*.json")
-// 	if err != nil {
-// 		fmt.Println("Error when calling CreateTemp:", err)
-// 		return
-// 	}
-
-// 	defer f.Close()
-// 	fmt.Println(f.Name())
-
-// 	// bodyBytes, err := io.ReadAll(r.Body)
-
-// 	// if err != nil {
-// 	// 	fmt.Println("Error in calling ReadAll():", err)
-// 	// 	return
-// 	// }
-
-// 	bodyBytes := []byte("Hell")
-
-// 	_, err = f.Write(bodyBytes)
-// 	if err != nil {
-// 		fmt.Println("Error when writing to data to file:", err)
-// 		return
-// 	}
-
-// }
-
-
 func CreateJSONFileAndStoreToIPFS(w http.ResponseWriter, r *http.Request) {
-	currentPath, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error when getting current directory:", err)
-		return
-	}
-
-	f, err := os.CreateTemp(currentPath, "tempfile-*.json")
-	if err != nil {
-		fmt.Println("Error when calling CreateTemp:", err)
-		return
-	}
-
-	defer f.Close()
-	defer os.Remove(f.Name())
+	defer r.Body.Close()
 
 	bodyBytes, err := io.ReadAll(r.Body)
-
-	
 	if err != nil {
-		fmt.Println("Error in calling ReadAll():", err)
+		http.Error(w, "failed to read request body", http.StatusBadRequest)
 		return
 	}
 
-	_, err = f.Write(bodyBytes)
+	tmpFile, err := os.CreateTemp(os.TempDir(), "vericred-*.json")
 	if err != nil {
-		fmt.Println("Error when writing to data to file:", err)
+		http.Error(w, "failed to create temp file", http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		_ = tmpFile.Close()
+		_ = os.Remove(tmpFile.Name())
+	}()
+
+	if _, err = tmpFile.Write(bodyBytes); err != nil {
+		http.Error(w, "failed to write temp file", http.StatusInternalServerError)
 		return
 	}
 
-	msg, err := UploadToIPFS(f.Name())
+	link, err := UploadToIPFS(tmpFile.Name())
 	if err != nil {
-		fmt.Println("Error:", err)
+		http.Error(w, "failed to upload to IPFS", http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(msg)
-	json.NewEncoder(w).Encode(map[string]string{"ipfslink": msg})
+
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(map[string]string{"ipfslink": strings.TrimSpace(link)})
 }
