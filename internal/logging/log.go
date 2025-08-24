5fdd7aa258c1004efed13a2f63fbab63ed205006
linuxer77 <harshitg187@gmail.com>
2025-08-24 18:55:28 +0530

    fixed logging

--- internal/logging/log.go
+++ internal/logging/log.go
@@ -4,12 +4,16 @@ import (
 	"log"
 	"os"
 )
+
 var Logger *log.Logger
 
-func Init()  {
+func Init() {
 	file, err := os.OpenFile("app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal(err)
+		// Read-only FS or other error: fallback to stdout
+		Logger = log.New(os.Stdout, "APP: ", log.Ldate|log.Ltime|log.Lshortfile)
+		Logger.Println("warning: could not open app.log; using stdout logger:", err)
+		return
 	}
 	Logger = log.New(file, "APP: ", log.Ldate|log.Ltime|log.Lshortfile)
 	Logger.Println("Logger initialized")
